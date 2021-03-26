e0ded00670061cdf80dd89ac0b611c656de4758f
bcharlotin1 <biancacharlotin1@gmail.com>
2021-03-26 14:29:22 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,30 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
   end
+
+  get "/" do
+    erb :index
+  end
+
+  get '/signup' do 
+    erb :signup
+  end
+
+  post '/signup' do 
+  end
+    # @user = User.find_by(username: params[:username]
+    
+    # if params[:username] && params[:email] && params[:password]
+    #   if @user
+    #     session[:user_id] = @user.id
+    #     redirect '/tweet'
+    #   end
+    #   end
+    # end
+  
+
 
 end
